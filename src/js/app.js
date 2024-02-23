0edf22c5ed9f1a19f9ca6e5e75fa36f598cb4d7d
Alexey Pozhalov <pozhalov7@mail.ru>
2024-02-23 17:39:01 +0300

    1-я версия формы для захода в чат

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import Widget from './widget.js';
+
+const widget = new Widget('ws://localhost:3000/ws');
+
+widget.createForm();
---
+++ webpack.prod.js
@@ -0,0 +1,12 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.config');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = merge(common, {
+    mode: 'production',
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin(),
+        ],
+    },
+});
